fba069afd3e013c9552805f3f9c557627876b680
Peter Kolski <peter@bildpeter.de>
2017-01-11 12:50:50 +0100

    namespace - Artifical Stupidity

--- shared/TalkerEliza.h
+++ shared/TalkerEliza.h
@@ -5,12 +5,15 @@
 #ifndef ELISAKORA_TALKERELIZA_H
 #define ELISAKORA_TALKERELIZA_H
 
+namespace ArStu
+{
 
-
-class TalkerEliza {
+class TalkerEliza
+{
 
 };
 
+}
 
 
 #endif //ELISAKORA_TALKERELIZA_H
