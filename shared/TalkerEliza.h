8af8c4e69b0187c808d2c4b1b077283caa65a4e5
Peter Kolski <peter@bildpeter.de>
2017-01-11 12:47:03 +0100

    Talker Class

---
+++ shared/TalkerEliza.h
@@ -0,0 +1,16 @@
+//
+// Created by Peter A. Kolski on 11.01.17.
+//
+
+#ifndef ELISAKORA_TALKERELIZA_H
+#define ELISAKORA_TALKERELIZA_H
+
+
+
+class TalkerEliza {
+
+};
+
+
+
+#endif //ELISAKORA_TALKERELIZA_H
