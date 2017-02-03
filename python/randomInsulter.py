3c9fdee98fb1eba22261c2a617529877c866a90c
Peter Kolski <peter@bildpeter.de>
2017-02-03 16:11:12 +0100

    Module for processing the keywords

--- python/randomInsulter.py
+++ python/randomInsulter.py
@@ -1,20 +1,32 @@
+
+# ======== Dependencies ===============
+import pandas as pd
 import nltk
 from nltk.tokenize import RegexpTokenizer   #For getting rid of punctuation
-tokenizer = RegexpTokenizer(r'\w+') #Special tokenizer
+from collections import defaultdict         # to get a dictionary of lists
+
+# ======== Special tokenizer ==========
+tokenizer = RegexpTokenizer(r'\w+')
 
 
+# =========== Functions ===============
 
+# -----------------------------------
 def getKeywords( textIn ):
     tokens_ = tokenizer.tokenize( textIn )
     tagged_ = nltk.pos_tag( tokens_ )
     result = [ word for word,pos in tagged_ if pos == 'NN']
     return result
 
+# -----------------------------------
+"""Get Pandas row, extract keywords and put it into a dictionary [Key->List]"""
 
-def putRowKeywordsToDict( row_, dict_ ):
+def putRowKeywordsToDict( row_ ):
+    dict_ = defaultdict(list)
     rowNum_ = row_[ 0 ]
     text_ = row_[ 1 ]
     keywords_ = getKeywords( text_ )
-    for key in keywords_:
-        dict_[ key ].append( rowNum_ )
+    for _key in keywords_:
+        dict_[ _key ].append( rowNum_ )
+    return dict_
 
