7e3461f313eb16d18ee9544fac85c0fe9807f1dc
Peter Kolski <peter@bildpeter.de>
2017-02-03 18:11:37 +0100

    Choosing random sentences

--- python/randomInsulter.py
+++ python/randomInsulter.py
@@ -4,6 +4,7 @@ import pandas as pd
 import nltk
 from nltk.tokenize import RegexpTokenizer   #For getting rid of punctuation
 from collections import defaultdict         # to get a dictionary of lists
+from random import randint
 
 # ======== Special tokenizer ==========
 tokenizer = RegexpTokenizer(r'\w+')
@@ -31,9 +32,15 @@ def putKeywordsToDict( dataFrame ):
     return dict_
 
 
-def answerFromText( textIn_, dict_, dataFrame_ ):
-    keysIn_ = getKeywords( textIn_ )
-    idKey = 1
-    idList = 0
-    rowNum = dict_[keysIn_[idKey]][idList]
-    return dataFrame_.iloc[rowNum]['Text']
+def answerFromText(textIn_, dict_, dataFrame_):
+    entriesNumDF = len(dataFrame_.index)
+    keysIn_ = getKeywords(textIn_)
+    idKey = 0
+
+    if ((len(keysIn_) != 0) and (keysIn_[idKey] in dict_)):
+        list = dict_[keysIn_[idKey]]
+        idList = randint(0, len(list) - 1)  # Random list entry
+        rowNum = list[idList]
+        return dataFrame_.iloc[rowNum]['Text']
+    else:
+        return dataFrame_.iloc[randint(0, entriesNumDF)]['Text']
