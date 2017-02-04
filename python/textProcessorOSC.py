81c4f3dce2fe49fe2ff8c6e9eb39115e83dd8be0
Peter Kolski <peter@bildpeter.de>
2017-02-04 23:01:02 +0100

    Refactoring

--- python/textProcessorOSC.py
+++ python/textProcessorOSC.py
@@ -1,30 +1,31 @@
 import insulterFromDatabase
 
 # =========== OSC ===============
-#osc INIT
+# osc INIT
 import OSC
 
-osc_sender_HostPort= ('127.0.0.1' , 22222) #OpenFrameworks
-osc_send_address = "/python_here"       #Sending   - python to OF
-osc_transform_address = "/transform"    #Recieving - OF to python
-oscReciever_HostPort = ('127.0.0.1' , 33333) #python
+oscSender_HostPort = ('127.0.0.1', 22222)  # to OpenFrameworks
+oscSenderAddress = "/answer"  # Sending   - python to OF
 
-# Python to openframeworks:
-oscSender = OSC.OSCClient()
-oscSender.connect(osc_sender_HostPort)
+oscReciever_HostPort = ('127.0.0.1', 33333)  # python
+oscRecieverAddress = "/original"  # Recieving - OF to python
 
-# OpenFrameworks to python:
-oscReceiver = OSC.ThreadingOSCServer(oscReciever_HostPort)
+# --- Python to openframeworks:
+oscSender = OSC.OSCClient( )
+oscSender.connect( oscSender_HostPort )
 
-#---
+# --- OpenFrameworks to python:
+oscReceiver = OSC.ThreadingOSCServer( oscReciever_HostPort )
+
+# ---
 # send ready message to openframeworks
-oscmsg = OSC.OSCMessage()
-oscmsg.setAddress(osc_send_address)
-oscmsg.append("READY");
-oscSender.send(oscmsg)
+oscmsg = OSC.OSCMessage( )
+oscmsg.setAddress( oscSenderAddress )
+oscmsg.append( "READY" )
+oscSender.send( oscmsg )
 
-#--------
-print "listening to:",osc_transform_address
+# --------
+print "listening to:", oscRecieverAddress
 # start osc listener:
-oscReceiver.addMsgHandler(osc_transform_address, transformMessageCallback)
-oscReceiver.serve_forever() # nothing after that works
+oscReceiver.addMsgHandler( oscRecieverAddress, transformMessageCallback )
+oscReceiver.serve_forever( )  # nothing after that works
