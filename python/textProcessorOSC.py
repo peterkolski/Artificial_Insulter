1a860be8c314df1c1cb4da4a4dc69a509be0c5e3
Peter Kolski <peter@bildpeter.de>
2017-02-04 23:45:23 +0100

    Answering to database

--- python/textProcessorOSC.py
+++ python/textProcessorOSC.py
@@ -1,8 +1,9 @@
-# import insulterFromDatabase
-
-# =========== OSC ===============
-# osc INIT
+import insulterFromDatabase as ai
+import pandas as pd
 import OSC
+
+dataTable = pd.read_csv('../data/ShitTalkTable.csv')
+resultDict = ai.putKeywordsToDict( dataTable )
 
 oscSender_HostPort = ('127.0.0.1', 22222)  # to OpenFrameworks
 oscSenderAddress_Txt = "/answer_text"  # Sending   - python to OF
@@ -23,17 +24,19 @@ message.setAddress( oscSenderAddress_Txt )
 message.append( "READY" )
 oscSender.send( message )
 
-def transformMessageCallback( addr, tags, stuff, source ):
+def replyToMessage( addr, tags, stuff, source ):
     input_sentence = stuff[ 0 ]
-    print "received text to transform:", input_sentence
-    message = OSC.OSCMessage( )
-    message.setAddress( oscSenderAddress_Txt )
-    message.append( "message recieved" )
-    oscSender.send( message )
+    print "Original:", input_sentence
+    answer = ai.answerFromText( input_sentence, resultDict, dataTable )
+    answer = ai.changeToMale( answer  )
+    messageAnswer = OSC.OSCMessage()
+    messageAnswer.setAddress( oscSenderAddress_Txt )
+    messageAnswer.append( answer )
+    oscSender.send( messageAnswer )
     return
 
 # --------
 print "listening to:", oscRecieverAddress_Txt
 # start osc listener:
-oscReceiver.addMsgHandler( oscRecieverAddress_Txt, transformMessageCallback )
+oscReceiver.addMsgHandler( oscRecieverAddress_Txt, replyToMessage )
 oscReceiver.serve_forever( )  # nothing after that works
