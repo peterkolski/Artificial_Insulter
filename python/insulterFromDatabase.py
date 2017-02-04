5e968ed97b661512829b83050d6de666c09e6014
Peter Kolski <peter@bildpeter.de>
2017-02-04 20:54:23 +0100

    renamed file

--- python/insulterFromDatabase.py
+++ python/insulterFromDatabase.py
@@ -82,3 +82,32 @@ def answerFromText(textIn_, dict_, dataFrame_):
         textAnswer dataFrame_.iloc[randint(0, entriesNumDF)]['Text']
     textAnswer += " "
     return textAnswer
+
+# =========== OSC ===============
+#osc INIT
+import OSC
+
+osc_to_of_address= ('127.0.0.1' , 22222) #OpenFrameworks
+osc_send_address = "/python_here"       #Sending   - python to OF
+osc_transform_address = "/transform"    #Recieving - OF to python
+osc_from_of_address = ('127.0.0.1' , 33333) #python
+
+# Python to openframeworks:
+oscSender = OSC.OSCClient()
+oscSender.connect(osc_to_of_address)
+
+# OpenFrameworks to python:
+oscReceiver = OSC.ThreadingOSCServer(osc_from_of_address)
+
+#---
+# send ready message to openframeworks
+oscmsg = OSC.OSCMessage()
+oscmsg.setAddress(osc_send_address)
+oscmsg.append("READY");
+oscSender.send(oscmsg)
+
+#--------
+print "listening to:",osc_transform_address
+# start osc listener:
+oscReceiver.addMsgHandler(osc_transform_address, transformMessageCallback)
+oscReceiver.serve_forever() # nothing after that works
