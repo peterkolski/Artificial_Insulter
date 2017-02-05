7555e6873d12cbcae4a8cc08b03ce46a1bdb36d7
Peter Kolski <peter@bildpeter.de>
2017-02-05 01:31:52 +0100

    Conversation input correct

--- main/TalkingEliza/src/ofBitchSkeletonApp.cpp
+++ main/TalkingEliza/src/ofBitchSkeletonApp.cpp
@@ -35,6 +35,7 @@ void ofBitchSkeletonApp::setup(){
     vidGrabber.setDesiredFrameRate( 15 );
     vidGrabber.initGrabber(camWidth, camHeight);
 
+    bitches.doConversation( "I hate you all", 0 );
 }
 
 //--------------------------------------------------------------
@@ -102,10 +103,7 @@ void ofBitchSkeletonApp::keyPressed(int key){
         {
             bitches.next();
             bitches.doConversation();
-
 //            bitchElisa.ask( textFromInput );
-            //    bitchKora.update();
-
             reset();
         }
     }
@@ -118,8 +116,7 @@ void ofBitchSkeletonApp::keyPressed(int key){
 
         if ( textFromInput != "" )
         {
-//            bitches.doConversation( textFromInput, 0 );
-//            bitchElisa.setTextToSend( textFromInput );
+            bitches.doConversation( textFromInput, 0 );
         }
         else
         {
@@ -213,7 +210,6 @@ void ofBitchSkeletonApp::drawText()
     ofDrawBitmapStringHighlight( bitches.getAnswerFromID( 1 ), 600, 200 );
 //    ofDrawBitmapStringHighlight( "OSC to Python: " + bitchElisa.getTextAsked(), 100, 220 );
 //    ofDrawBitmapStringHighlight( "OSC from Python: " + bitchElisa.getAnswer(), 100, 240 );
-//    ofDrawBitmapStringHighlight( "OSC: " + bitchKora.getAnswer(), 600, 220 );
 
     ofDrawBitmapStringHighlight( "voice: " + voice, ofGetWidth() - 180, ofGetHeight() - 20 );
     ofDrawBitmapStringHighlight( "Said to Elisa: " + textFromInput, 10, ofGetHeight() - 20 );
