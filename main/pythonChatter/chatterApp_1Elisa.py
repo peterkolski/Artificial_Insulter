0350320081348f582e8596135514d3c0083ad30c
Peter Kolski <peter@bildpeter.de>
2017-02-09 15:23:09 +0100

    dataTable as parameter

--- main/pythonChatter/chatterApp_1Elisa.py
+++ main/pythonChatter/chatterApp_1Elisa.py
@@ -3,35 +3,33 @@ import pandas as pd
 import OSC
 from chatterbot import ChatBot
 
+dataTableShitTalk = pd.read_csv('../../data/ShitTalkTable.csv')
 
 host = '127.0.0.1'
-portSender = 22223
-portReciever = 33334
+portSender = 22222
+portReciever = 33333
 
-
-# =================================
-# =================================
-dataTable = pd.read_csv('../../data/ShitTalkTable.csv')
-resultDict = ai.putKeywordsToDict( dataTable )
 
 # chatbot = ChatBot( 'Elisa Test', trainer='chatterbot.trainers.ChatterBotCorpusTrainer' )
-chatbot = ChatBot( 'Kora', trainer='chatterbot.trainers.ChatterBotCorpusTrainer' )
+chatbot = ChatBot( 'Kora',
+                    trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
+                    storage_adapter="chatterbot.storage.JsonFileStorageAdapter",
+                    read_only=True,
+                    database = "./databasePeter.json" )
 # chatbot.train("chatterbot.corpus.english") # Train based on the english corpus
 # chatbot.get_response("Hello, how are you today?")
 
-# =================================
-# === CHATTER CLASS
-# =================================
 
 class Chatter:
     oscSender               = OSC.OSCClient( )
     oscSenderAddress_Txt    = "/answer_text"     # Python to OF
     oscRecieverAddress_Txt  = "/original_text"   # OF to python
 
-    def __init__(self, host, portSender, portReciever):
+    def __init__(self, host, portSender, portReciever, dataTable):
         self.host_ = host
         self.portSender_ = portSender
         self.portReciever_ = portReciever
+        self.dataTable      = dataTable
         self.oscSender_HostPort = (self.host_, self.portSender_)  # to OpenFrameworks
         self.oscReciever_HostPort = (self.host_, self.portReciever_)  # python
         self.counter = 0
@@ -39,6 +37,7 @@ class Chatter:
         print self.oscReciever_HostPort
         self.oscSender.connect( self.oscSender_HostPort )
         self.oscReceiver = OSC.ThreadingOSCServer( self.oscReciever_HostPort )
+        self.resultDict = ai.putKeywordsToDict( dataTable )
 
     def send(self, text ):
         messageOSC = OSC.OSCMessage( )
@@ -52,23 +51,22 @@ class Chatter:
         self.counter += 1
         # answer = str( self.counter ) + " "
         answer = ""
-        # if ( self.counter % 2 == 0 ):        # TODO switching between the chatters
-        #     # answer += "ChatterBot | "
-        #     if (input_sentence != ""):
-        answer += str( chatbot.get_response( input_sentence ) )
-        # else:
-        #     # answer += "DataInsulter | "
-        #     answerTMP = ai.answerFromText( input_sentence, resultDict, dataTable )
-        #     answer += ai.changeToMale( answerTMP )
+        if ( self.counter % 2 == 0 ):        # TODO switching between the chatters
+            # answer += "ChatterBot | "
+            if (input_sentence != ""):
+                answer += str( chatbot.get_response( input_sentence ) )
+        else:
+            # answer += "DataInsulter | "
+            answerTMP = ai.answerFromText( input_sentence, self.resultDict, self.dataTable )
+            answer += ai.changeToMale( answerTMP )
         print "Answer: ", answer
         self.send( answer )
         return
 
 # =================================
-# === Run program
 # =================================
 
-chatterElisa = Chatter( host, portSender, portReciever )
+chatterElisa = Chatter( host, portSender, portReciever, dataTableShitTalk )
 
 # --- send ready message to openframeworks
 chatterElisa.send( "READY" )
