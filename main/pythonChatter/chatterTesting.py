87d61ca70bde3d490981571035ca7413be5c8110
Peter Kolski <peter@bildpeter.de>
2017-02-09 18:28:06 +0100

    Test output to verify learned stuff

---
+++ main/pythonChatter/chatterTesting.py
@@ -0,0 +1,30 @@
+from chatterbot import ChatBot
+import logging
+
+
+# Uncomment the following line to enable verbose logging
+# logging.basicConfig(level=logging.INFO)
+
+# Create a new instance of a ChatBot
+bot = ChatBot("Terminal",
+    storage_adapter="chatterbot.storage.JsonFileStorageAdapter",
+    logic_adapters=[ "chatterbot.logic.BestMatch"],
+    input_adapter="chatterbot.input.TerminalAdapter",
+    output_adapter="chatterbot.output.TerminalAdapter",
+    filters=["chatterbot.filters.RepetitiveResponseFilter"],
+    database="./databaseTest.json",
+    read_only=True
+)
+
+print("Type something to begin...")
+
+# The following loop will execute each time the user enters input
+while True:
+    try:
+        # We pass None to this method because the parameter
+        # is not used by the TerminalAdapter
+        bot_input = bot.get_response(None)
+
+    # Press ctrl-c or ctrl-d on the keyboard to exit
+    except (KeyboardInterrupt, EOFError, SystemExit):
+        break
