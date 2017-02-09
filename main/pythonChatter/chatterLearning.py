d9dec8c546c529350a1ae959c927ba9030f184ca
Peter Kolski <peter@bildpeter.de>
2017-02-09 18:27:35 +0100

    Learning works

--- main/pythonChatter/chatterLearning.py
+++ main/pythonChatter/chatterLearning.py
@@ -15,9 +15,7 @@ element from the user.
 bot = ChatBot(
     'Feedback Learning Bot',
     storage_adapter='chatterbot.storage.JsonFileStorageAdapter',
-    logic_adapters=[
-        'chatterbot.logic.BestMatch'
-    ],
+    logic_adapters=[ 'chatterbot.logic.BestMatch'],
     database = "./databaseTest.json",
     input_adapter='chatterbot.input.TerminalAdapter',
     output_adapter='chatterbot.output.TerminalAdapter'
@@ -41,27 +39,32 @@ def get_feedback():
         return get_feedback()
 
 
-print('Type something to begin...')
+# print('Type something to begin...')
 
+# ===============================================
 # The following loop will execute each time the user enters input
 while True:
     try:
+        print "Type a statement (KEYWORD): "
         input_statement = bot.input.process_input_statement()
+        print "Type a response (sentence): "
+        response        = bot.input.process_input_statement()
         # print bot.generate_response( input_statement )
-        statement, response, someNr = bot.generate_response( input_statement )
+        # statement, response, someNr = bot.generate_response( input_statement )
 
-        print('\n Is "{}" this a coherent response to "{}"? \n'.format(response, input_statement))
+        # print('\n Is "{}" this a coherent response to "{}"? \n'.format(response, input_statement))
 
-        if get_feedback():
-            bot.learn_response(response,input_statement)
+        # if get_feedback():
+        bot.learn_response(response,input_statement)
 
-        bot.output.process_response(response)
+        print "Learned!\n"
+        # bot.output.process_response(response)
 
         # Update the conversation history for the bot
         # It is important that this happens last, after the learning step
         bot.conversation_sessions.update(
             bot.default_session.id_string,
-            (statement, response, )
+            (input_statement, response, )
         )
 
     # Press ctrl-c or ctrl-d on the keyboard to exit
