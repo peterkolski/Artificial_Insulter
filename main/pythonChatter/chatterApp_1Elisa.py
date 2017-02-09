3e0909f65588d8e5d458aa55fb1ec14e5e626444
Peter Kolski <peter@bildpeter.de>
2017-02-09 15:45:37 +0100

    chatbot as parameter

--- main/pythonChatter/chatterApp_1Elisa.py
+++ main/pythonChatter/chatterApp_1Elisa.py
@@ -11,25 +11,29 @@ portReciever = 33333
 
 
 # chatbot = ChatBot( 'Elisa Test', trainer='chatterbot.trainers.ChatterBotCorpusTrainer' )
-chatbot = ChatBot( 'Kora',
-                    trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
-                    storage_adapter="chatterbot.storage.JsonFileStorageAdapter",
-                    read_only=True,
-                    database = "./databasePeter.json" )
+chatterbotInstance = ChatBot('Kora',
+                     trainer='chatterbot.trainers.ChatterBotCorpusTrainer',
+                     storage_adapter="chatterbot.storage.JsonFileStorageAdapter",
+                     read_only=True,
+                     database = "./databasePeter.json")
 # chatbot.train("chatterbot.corpus.english") # Train based on the english corpus
 # chatbot.get_response("Hello, how are you today?")
 
+# =================================
+# === CHATTER CLASS
+# =================================
 
 class Chatter:
     oscSender               = OSC.OSCClient( )
     oscSenderAddress_Txt    = "/answer_text"     # Python to OF
     oscRecieverAddress_Txt  = "/original_text"   # OF to python
 
-    def __init__(self, host, portSender, portReciever, dataTable):
+    def __init__(self, host, portSender, portReciever, dataTable, chatterbot):
         self.host_ = host
         self.portSender_ = portSender
         self.portReciever_ = portReciever
-        self.dataTable      = dataTable
+        self.dataTable_      = dataTable
+        self.chatterbot_     = chatterbot
         self.oscSender_HostPort = (self.host_, self.portSender_)  # to OpenFrameworks
         self.oscReciever_HostPort = (self.host_, self.portReciever_)  # python
         self.counter = 0
@@ -54,10 +58,10 @@ class Chatter:
         if ( self.counter % 2 == 0 ):        # TODO switching between the chatters
             # answer += "ChatterBot | "
             if (input_sentence != ""):
-                answer += str( chatbot.get_response( input_sentence ) )
+                answer += str(self.chatterbot_.get_response(input_sentence))
         else:
             # answer += "DataInsulter | "
-            answerTMP = ai.answerFromText( input_sentence, self.resultDict, self.dataTable )
+            answerTMP = ai.answerFromText(input_sentence, self.resultDict, self.dataTable_)
             answer += ai.changeToMale( answerTMP )
         print "Answer: ", answer
         self.send( answer )
@@ -66,7 +70,7 @@ class Chatter:
 # =================================
 # =================================
 
-chatterElisa = Chatter( host, portSender, portReciever, dataTableShitTalk )
+chatterElisa = Chatter( host, portSender, portReciever, dataTableShitTalk, chatterbotInstance )
 
 # --- send ready message to openframeworks
 chatterElisa.send( "READY" )
